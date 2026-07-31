00f8872e3960b41185df889adaa1ad5559c5b842
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2026-07-31 18:33:49 +0700

    fix: import DiscordReportModule in DiscordOpsModule

--- apps/discord-bot/src/modules/discord-ops/discord-ops.module.ts
+++ apps/discord-bot/src/modules/discord-ops/discord-ops.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../../shared/common/common.module';
 import { DiscordChatModule } from '../discord-chat/discord-chat.module';
+import { DiscordReportModule } from '../discord-chat/discord-report.module';
 import { DiscordStudyReminderModule } from '../discord-study-reminder/discord-study-reminder.module';
 import { DiscordOpsController } from './discord-ops.controller';
 
 @Module({
-  imports: [CommonModule, DiscordChatModule, DiscordStudyReminderModule],
+  imports: [CommonModule, DiscordChatModule, DiscordReportModule, DiscordStudyReminderModule],
   controllers: [DiscordOpsController],
 })
 export class DiscordOpsModule {}
